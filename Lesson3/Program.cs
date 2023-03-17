42a25107d2acc2b0c3c1f4bd898339823d05fd1f
Vladislav Gorokhov <v.gorokhov@skipp.pro>
2023-03-17 12:36:56 +0300

    Task 2

--- Lesson3/Program.cs
+++ Lesson3/Program.cs
@@ -3,23 +3,8 @@
 var employees = DataGenerator.GenerateEmployees();
 var departments = DataGenerator.GenerateDepartments();
 
-// T1: {FirstName} {FirstName}, {Title1} {Title2}
+// T2: {FirstName} {FirstName} (only men)
 
-// 1, 2, 3, 4 -> Select(x => x + 1) -> 2, 3, 4, 5
-
-var results = employees
-    .Select(employee => 
-        new
-        {
-            Employee = employee, 
-            Titles = string.Join(" ", employee.Titles.Select(title => title.Value))
-        })
-    .Select(x => $"{x.Employee.FirstName} {x.Employee.LastName}, {x.Titles}");
-
-foreach (var result in results)
-{
-    Console.WriteLine(result);
-}
 
 
 
