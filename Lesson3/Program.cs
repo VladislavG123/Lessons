53d77e8554614cebdc816c58898edda28521aadf
Vladislav Gorokhov <v.gorokhov@skipp.pro>
2023-03-17 13:09:48 +0300

    task 2

--- Lesson3/Program.cs
+++ Lesson3/Program.cs
@@ -5,6 +5,23 @@ var departments = DataGenerator.GenerateDepartments();
 
 // T2: {FirstName} {FirstName} (only men)
 
+// 1, 2, 3 -> Where(x => x > 1) -> 2, 3 
+// 1, 2, 3 -> Where(x => x % 2 == 0) -> 2 
+// 1, 2, 3
+//
+// 2 -> [2]
+// 
+
+// 1, 2, 3 -> Select(x => x * 2) -> 2, 4, 6
+
+
+var results = employees
+    .Where(x => x.Sex)
+    .Select(x => x.FirstName + " " + x.LastName + " " + (x.Sex ? "Male" : "Female"));
+
+
+foreach(var res in results) Console.WriteLine(res);
+
 
 
 
