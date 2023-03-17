3632e3da17f0ad8309b5d2719de651027b2fd891
Vladislav Gorokhov <v.gorokhov@skipp.pro>
2023-03-17 12:34:23 +0300

    T1: {FirstName} {FirstName}, {Title1} {Title2}

--- Lesson3/Program.cs
+++ Lesson3/Program.cs
@@ -3,8 +3,23 @@
 var employees = DataGenerator.GenerateEmployees();
 var departments = DataGenerator.GenerateDepartments();
 
-// T1: {FirstName} {FirstName}, {Title1} {Title1}
+// T1: {FirstName} {FirstName}, {Title1} {Title2}
 
-Console.Read();
+// 1, 2, 3, 4 -> Select(x => x + 1) -> 2, 3, 4, 5
+
+var results = employees
+    .Select(employee => 
+        new
+        {
+            Employee = employee, 
+            Titles = string.Join(" ", employee.Titles.Select(title => title.Value))
+        })
+    .Select(x => $"{x.Employee.FirstName} {x.Employee.LastName}, {x.Titles}");
+
+foreach (var result in results)
+{
+    Console.WriteLine(result);
+}
+
 
 
